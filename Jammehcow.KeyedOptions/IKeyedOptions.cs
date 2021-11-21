eb71a1ba50df271d59a023309af4de8a9faf5cf6
James Upjohn <jammehcow@jammehcow.co.nz>
2021-11-21 16:50:19 +1300

    feat(option): create keyed option interface

---
+++ Jammehcow.KeyedOptions/IKeyedOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Jammehcow.KeyedOptions
+{
+    /// <summary>
+    /// An interface allowing options to provide their own section key
+    /// </summary>
+    public interface IKeyedOptions
+    {
+        /// <summary>
+        /// The section key in the appsettings file
+        /// (e.g. {"MySection":{"A": "B"}} would be "MySection")
+        /// </summary>
+        public string SectionKey { get; }
+    }
+}
